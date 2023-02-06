36790db93fd5f64b5a267290566cb425d58951e1
Guilherme <guilhermegsilverio@gmail.com>
2023-02-06 17:42:08 -0300

    Find Category by id, exception handler

--- backend/src/main/java/com/gsilverio/barber/resources/UserResource.java
+++ backend/src/main/java/com/gsilverio/barber/resources/UserResource.java
@@ -7,6 +7,7 @@ import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +16,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping(value="/users")
 public class UserResource {
     @Autowired
     private UserService userService;
@@ -23,5 +24,10 @@ public class UserResource {
     public ResponseEntity<List<UserDTO>> findAll(){
         List<UserDTO> user = userService.findAll();
         return ResponseEntity.ok().body(user);
+    }
+    @GetMapping(value="/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable Long id){
+        UserDTO dto = userService.findById(id);
+        return ResponseEntity.ok().body(dto);
     }
 }
--- backend/src/main/java/com/gsilverio/barber/services/UserService.java
+++ backend/src/main/java/com/gsilverio/barber/services/UserService.java
@@ -3,12 +3,14 @@ package com.gsilverio.barber.services;
 import com.gsilverio.barber.dto.UserDTO;
 import com.gsilverio.barber.entities.User;
 import com.gsilverio.barber.repositories.UserRepository;
+import com.gsilverio.barber.services.exceptions.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -20,5 +22,13 @@ public class UserService {
         List<User> list = userRepository.findAll();
         List<UserDTO> listDto = list.stream().map(x->new UserDTO(x)).collect(Collectors.toList());
         return listDto;
+    }
+    @Transactional(readOnly = true)
+    public UserDTO findById(Long id) {
+        Optional<User> obj = userRepository.findById(id);
+
+        //orElseThrow lanca uma execao caso nao tenha uma entitadade no obj
+        User entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+        return new UserDTO(entity);
     }
 }
